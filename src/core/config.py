933ac6f8eda68efbb417479502c0d13b14f1faac
Akanksha <ak.t.kree@gmail.com>
2026-03-03 16:20:52 +0530

    feat: setting up dvc

---
+++ src/core/config.py
@@ -0,0 +1,14 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from functools import lru_cache
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        case_sensitive=False
+    )
+
+
+@lru_cache
+def get_settings() -> Settings:
+    return Settings
